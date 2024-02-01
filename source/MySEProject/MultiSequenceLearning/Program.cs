27e098ad49d121495502179f06b6cd012e4f8b7a
Babar-Ali-IT <116679936+Babar-Ali-IT@users.noreply.github.com>
2024-02-01 23:48:30 +0100

    Creating class and calling into main

--- source/MySEProject/MultiSequenceLearning/Program.cs
+++ source/MySEProject/MultiSequenceLearning/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Strting MultiSequence Project");
+﻿using MultiSequenceLearning;
+Multisequencelearning.myproject();
+Console.WriteLine("Done");
