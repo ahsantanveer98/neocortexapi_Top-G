7a39143beb22b7e9e158def266081621b5025011
Babar-Ali-IT <116679936+Babar-Ali-IT@users.noreply.github.com>
2024-02-27 21:38:15 +0100

    Code update

--- source/MySEProject/MultiSequenceLearning/ProjectStarter.cs
+++ source/MySEProject/MultiSequenceLearning/ProjectStarter.cs
@@ -9,21 +9,14 @@ namespace ProjectMultiSequenceLearning
     public class ProjectStarter
     {
         /// <summary>
-        /// Training File Paths For Images and Sequences
-        /// </summary>
-        ////
-
-
-
-        /// <summary>
         /// Print Message During Startup of Program
         /// </summary>
         /// <param name="None"></param>
         public static void startingproject()
         {
             Console.WriteLine("\n");
-            // Set the Foreground color to White
-            Console.ForegroundColor = ConsoleColor.White;
+            // Set the Foreground color to green
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("######## ########    ###    ##     ##    ########   ####     #########             ######    ");
             Console.WriteLine("   ##    ##         ## ##   ###   ###       ##     ##  ##    ##      ##           ##         ");
             Console.WriteLine("   ##    ##        ##   ##  #### ####       ##    ##    ##   ##      ##          ##         ");
@@ -34,18 +27,18 @@ namespace ProjectMultiSequenceLearning
 
             Console.WriteLine("\n\n\n");
 
-            // Set the Foreground color to Yellow
+            // Set the Foreground color to dark yellow
             Console.ForegroundColor = ConsoleColor.DarkYellow;
 
-            Console.WriteLine("**************          Multi Sequence Learning Project              ************** \n ");
-            Console.WriteLine("**************  Project - 1 Multi Sequence Learning - Cancer_Prediction     ************** ");
-            Console.WriteLine("**************  Project - 2 Multi Sequence Learning - Power_Prediction   ************** ");
-            Console.WriteLine("**************  Project - 3 Multi Sequence Learning - Heart_Disease_Prediction      ************** ");
+            Console.WriteLine("**************          Approve Prediction of Multi Sequence Learning               ************** \n ");
+            Console.WriteLine("**************  Scenario - 1 - Cancer_Prediction     ************** ");
+            Console.WriteLine("**************  Scenario - 2 - Power_Prediction   ************** ");
+            Console.WriteLine("**************  Scenario - 3 - Heart_Disease_Prediction      ************** ");
 
             Console.WriteLine("\n");
-            Console.WriteLine("Please Enter A Project Number to Continue with MultiSequence Experiment");
+            Console.WriteLine("Please Enter a Scenario Number to Continue with MultiSequence Experiment");
 
-            // Set the Foreground color to White
+            // Set the Foreground color to white
             Console.ForegroundColor = ConsoleColor.White;
         }
 
@@ -59,34 +52,30 @@ namespace ProjectMultiSequenceLearning
             switch (UserInput)
             {
                 case 1:
-                {
+                
                         Console.WriteLine("User Selected MultiSequence Experiment - Cancer_Prediction\n");
-                        //CancerPredictionTraining.cancerprediction();
-                        //HelperMethod_Numbers multiSeqLearn_Numbers = new HelperMethod_Numbers();
-                        //multiSeqLearn_Numbers.MultiSequenceLearning_Numbers();              
-                }
+                                     
+                
                 break;
 
                 case 2:
-                {
+                
                         Console.WriteLine("User Selected MultiSequence Experiment - Power_Prediction\n");
-                        //HelperMethod_Numbers multiSeqLearn_Numbers = new HelperMethod_Numbers();
-                        //multiSeqLearn_Numbers.MultiSequenceLearning_Numbers();              
-                }
+                                     
+                
                 break;
 
                 case 3:
-                {
+                
                         Console.WriteLine("User Selected MultiSequence Experiment - Heart_Disease_Prediction\n");
-                        //HelperMethod_Numbers multiSeqLearn_Numbers = new HelperMethod_Numbers();
-                        //multiSeqLearn_Numbers.MultiSequenceLearning_Numbers();              
-                }
+                                     
+                
                 break;
 
                 default:
-                {
-                     Console.WriteLine("User Entered Invalid Option");
-                }
+                
+                        Console.WriteLine("User Entered Invalid Option");
+                
                 break;
             }
         }
