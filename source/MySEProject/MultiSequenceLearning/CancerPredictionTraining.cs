b8a0a9c178f91e0423b23772e3f890bf11a4d27f
Babar-Ali-IT <116679936+Babar-Ali-IT@users.noreply.github.com>
2024-03-08 16:47:01 +0100

    Adding some lines of code

--- source/MySEProject/MultiSequenceLearning/CancerPredictionTraining.cs
+++ source/MySEProject/MultiSequenceLearning/CancerPredictionTraining.cs
@@ -38,4 +38,47 @@ namespace ProjectMultiSequenceLearning
         /// <param name="CSVFilePath"></param>
         /// <returns></returns>
     }
+    public static List<Dictionary<string, string>> ReadDataFromTrainingCSV(string CSVFilePath)
+    {
+        List<Dictionary<string, string>> SequencesCollection = new List<Dictionary<string, string>>();
+
+        int keyForUniqueIndexes = 0;
+
+        if (File.Exists(CSVFilePath))
+        {
+            using (StreamReader sr = new StreamReader(CSVFilePath))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    var line = sr.ReadLine();
+                    if (line != null)
+                    {
+                        string[] values = line.Split(",");
+
+                        Dictionary<string, string> Sequence = new Dictionary<string, string>();
+
+                        string label = values[1];
+                        string sequenceString = values[0];
+
+                        foreach (var alphabet in sequenceString)
+                        {
+                            keyForUniqueIndexes++;
+                            if (Sequence.ContainsKey(alphabet.ToString()))
+                            {
+                                var newKey = alphabet.ToString() + "," + keyForUniqueIndexes;
+                                Sequence.Add(newKey, label);
+                            }
+                            else
+                            {
+                                Sequence.Add(alphabet.ToString(), label);
+                            }
+                        }
+                        SequencesCollection.Add(Sequence);
+                    }
+                }
+            }
+            return SequencesCollection;
+        }
+        return SequencesCollection;
+    }
 }
