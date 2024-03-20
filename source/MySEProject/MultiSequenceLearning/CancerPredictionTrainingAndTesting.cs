9329885c35319a8a74e7623480297b336359962a
Babar-Ali-IT <116679936+Babar-Ali-IT@users.noreply.github.com>
2024-03-20 02:30:56 +0100

    updated code

--- source/MySEProject/MultiSequenceLearning/CancerPredictionTrainingAndTesting.cs
+++ source/MySEProject/MultiSequenceLearning/CancerPredictionTrainingAndTesting.cs
@@ -9,221 +9,32 @@ using System.Linq;
 using System.Globalization;
 using System.Drawing;
 
-
 using NeoCortexApi;
 using NeoCortexApi.Classifiers;
 using NeoCortexApi.Entities;
 using NeoCortexApi.Network;
 using NeoCortexApi.Encoders;
 using NeoCortexApi.Utility;
-
 using HtmImageEncoder;
-
 using Daenet.ImageBinarizerLib.Entities;
 using Daenet.ImageBinarizerLib;
-
 using Newtonsoft.Json;
-
 using SkiaSharp;
-
-
 
 namespace ProjectMultiSequenceLearning
 {
     public class CancerPredictionTrainingAndTesting
     {
         /// <summary>
-        ///     Fetch Data Sequence from the File 
+        /// Runs MultiSequence Learning Experiment - To Carry out Sequence Learning for Cancer Prediction.
         /// </summary>
-        /// <param name="dataFilePath"></param>
-        /// <returns></returns>
-        public static List<Dictionary<string, string>> ReadSequencesDataFromCSV(string dataFilePath)
+        /// <param name="predictionScenario"></param>
+        /// <param name="trainingDataFilePath"></param>
+        /// <param name="testingDataFilePath"></param>
+        public void RunMultiSequenceLearningExperiment(int predictionScenario, string trainingDataFilePath, string testingDataFilePath)
         {
-            List<Dictionary<string, string>> SequencesCollection = new List<Dictionary<string, string>>();
-
-            int keyForUniqueIndexes = 0;
-
-            if (File.Exists(dataFilePath))
-            {
-                using (StreamReader sr = new StreamReader(dataFilePath))
-                {
-                    while (sr.Peek() >= 0)
-                    {
-                        var line = sr.ReadLine();
-                        if (line != null)
-                        {
-                            string[] values = line.Split("\t");
-
-                            Dictionary<string, string> Sequence = new Dictionary<string, string>();
-
-                            string label = values[1];
-                            string sequenceString = values[0];
-
-                            foreach (var alphabet in sequenceString)
-                            {
-                                keyForUniqueIndexes++;
-                                if (Sequence.ContainsKey(alphabet.ToString()))
-                                {
-                                    var newKey = alphabet.ToString() + "," + keyForUniqueIndexes;
-                                    Sequence.Add(newKey, label);
-                                }
-                                else
-                                {
-                                    Sequence.Add(alphabet.ToString(), label);
-                                }
-                            }
-                            SequencesCollection.Add(Sequence);
-                        }
-                    }
-                }
-                return SequencesCollection;
-            }
-            return SequencesCollection;
-        }
-
-        /// <summary>
-        ///     Encoding Cancer_Alphabetic Sequences
-        /// </summary>
-        /// <param name="trainingData"></param>
-        /// <returns></returns>
-        /// 
-        public static List<Dictionary<string, int[]>> TrainEncodeSequencesFromCSV(List<Dictionary<string, string>> trainingData)
-        {
-            List<Dictionary<string, int[]>> ListOfEncodedTrainingSDR = new List<Dictionary<string, int[]>>();
-
-            EncoderBase encoder_Alphabets = FetchAlphabetEncoder();
-
-            foreach (var sequence in trainingData)
-            {
-                int keyForUniqueIndex = 0;
-                var tempDictionary = new Dictionary<string, int[]>();
-
-                foreach (var element in sequence)
-                {
-                    keyForUniqueIndex++;
-                    var elementLabel = element.Key + "," + element.Value;
-                    var elementKey = element.Key;
-                    int[] sdr = new int[0];
-                    sdr = sdr.Concat(encoder_Alphabets.Encode(char.ToUpper(element.Key.ElementAt(0)) - 64)).ToArray();
-
-                    if (tempDictionary.ContainsKey(elementLabel))
-                    {
-                        var newKey = elementLabel + "," + keyForUniqueIndex;
-                        tempDictionary.Add(newKey, sdr);
-                    }
-                    else
-                    {
-                        tempDictionary.Add(elementLabel, sdr);
-                    }
-                }
-                ListOfEncodedTrainingSDR.Add(tempDictionary);
-            }
-            return ListOfEncodedTrainingSDR;
-        }
-
-        /// <summary>
-        ///         FetchAlphabetEncoder 
-        /// </summary>
-        /// <returns> SCALAR ENCODERS</returns>
-        public static EncoderBase FetchAlphabetEncoder()
-        {
-            EncoderBase AlphabetEncoder = new ScalarEncoder(new Dictionary<string, object>()
-                {
-                    { "W", 5},
-                    { "N", 31},
-                    { "Radius", -1.0},
-                    { "MinVal", (double)1},
-                    { "Periodic", true},
-                    { "Name", "scalar"},
-                    { "ClipInput", false},
-                    { "MaxVal", (double)27}
-                });
-            return AlphabetEncoder;
-        }
-
-        /// <summary>
-        /// HTM Config for creating Connections
-        /// </summary>
-        /// <param name="inputBits">input bits</param>
-        /// <param name="numColumns">number of columns</param>
-        /// <returns>Object of HTMConfig</returns>
-        public static HtmConfig FetchHTMConfig(int inputBits, int numColumns)
-        {
-            HtmConfig cfg = new HtmConfig(new int[] { inputBits }, new int[] { numColumns })
-            {
-                Random = new ThreadSafeRandom(42),
-
-                CellsPerColumn = 25,
-                GlobalInhibition = true,
-                LocalAreaDensity = -1,
-                NumActiveColumnsPerInhArea = 0.02 * numColumns,
-                PotentialRadius = 65, /*(int)(0.15 * inputBits),*/
-                InhibitionRadius = 15,
-
-                MaxBoost = 10.0,
-                DutyCyclePeriod = 25,
-                MinPctOverlapDutyCycles = 0.75,
-                MaxSynapsesPerSegment = 128, /*(int)(0.02 * numColumns),*/
-
-                ActivationThreshold = 15,
-                ConnectedPermanence = 0.5,
-
-                // Learning is slower than forgetting in this case.
-                PermanenceDecrement = 0.25,
-                PermanenceIncrement = 0.15,
-
-                // Used by punishing of segments.
-                PredictedSegmentDecrement = 0.1,
-            };
-
-            return cfg;
-        }
-
-        /// <summary>
-        ///     Fetch Testing Data Sequence from the File 
-        /// </summary>
-        /// <param name="dataFilePath"></param>
-        /// <returns></returns>
-        public static List<string> ReadTestingSequencesDataFromCSV(string dataFilePath)
-        {
-            List<string> ListOfTestingSeqData = new List<string>();
-
-            if (File.Exists(dataFilePath))
-            {
-                using (StreamReader sr = new StreamReader(dataFilePath))
-                {
-                    while (sr.Peek() >= 0)
-                    {
-                        var line = sr.ReadLine();
-                        if (line != null)
-                        {
-                            ListOfTestingSeqData.Add(line);
-                        }
-                    }
-                }
-            }
-            return ListOfTestingSeqData;
-        }
-
-        /// <summary>
-        ///     Fetch Testing Data Sequence from the File 
-        /// </summary>
-        /// <param name="testingAlphabet"></param>
-        /// <returns></returns>
-        public static int[] PredictTestingSequence(string testingAlphabet)
-        {
-            EncoderBase alphabetEncoder = FetchAlphabetEncoder();
-            return alphabetEncoder.Encode(char.ToUpper(testingAlphabet.ElementAt(0)) - 64);
-        }
-
-        /// <summary>
-        /// Runs MultiSequence Learning Experiment - To Carry out Sequence Learning for Alphabets.
-        /// </summary>
-        /// <param name="datafilepath"></param>
-        public void RunMultiSequenceLearningExperiment(string trainingDataFilePath, string testingDataFilePath)
-        {
-            var trainingData = ReadSequencesDataFromCSV(trainingDataFilePath);
-            var trainingDataProcessed = TrainEncodeSequencesFromCSV(trainingData);
+            var trainingData = Helper.ReadSequencesDataFromCSV(trainingDataFilePath);
+            var trainingDataProcessed = Helper.TrainEncodeSequencesFromCSV(trainingData, predictionScenario);
 
 
             Console.WriteLine("Variables are being trained Please Wait....");
@@ -239,18 +50,19 @@ namespace ProjectMultiSequenceLearning
             //var trained_Classifier = trained_HTM_model.Values.ElementAt(0);
 
             Console.WriteLine("Ready to Predict.....");
-            var testingData = ReadTestingSequencesDataFromCSV(testingDataFilePath);
+            var testingData = Helper.ReadTestingSequencesDataFromCSV(testingDataFilePath);
 
             //List<Report> reports = new List<Report>();
             //Report report = new Report();
             foreach (var seqData in testingData)
             {
-
+                Console.WriteLine("------------------------------");
                 Console.WriteLine($"Sequence {seqData}");
-                var accuracy = PredictElementAccuracy(predictor, seqData);
+                predictor.Reset();
+                var accuracy = PredictElementAccuracy(predictor, seqData, predictionScenario);
                 Console.WriteLine($"Accuracy {accuracy}");
-                //Console.WriteLine($"Predicted Class : {predictedSequence.possibleClass.Split("_")[0]}, accuracy: {accuracy}");
             }
+            Console.WriteLine("------------------------------");
 
 
 
@@ -279,12 +91,13 @@ namespace ProjectMultiSequenceLearning
         }
 
         /// <summary>
-        /// Takes predicted model, subsequence and generates report stating accuracy
+        /// Takes predicted model, subsequence and predict accuracy
         /// </summary>
         /// <param name="predictor">Object of Predictor</param>
-        /// <param name="list">sub-sequence to be tested</param>
+        /// <param name="sequenceData">sequence to be tested</param>
+        /// <param name="predictionScenario">Prediction Scenario</param>
         /// <returns>accuracy of predicting elements in %</returns>
-        private static double PredictElementAccuracy(Predictor predictor, string sequenceData)
+        private static double PredictElementAccuracy(Predictor predictor, string sequenceData, int predictionScenario)
         {
             int matchCount = 0;
             int predictions = 0;
@@ -325,7 +138,7 @@ namespace ProjectMultiSequenceLearning
                 {
                     //var element = sequenceData.ElementAt(i);
                     Console.WriteLine($"Element {prevSeqItem}");
-                    var elementSDR = PredictTestingSequence(prevSeqItem.ToString());
+                    var elementSDR = Helper.EncodeTestingElement(prevSeqItem.ToString(), predictionScenario);
                     var classifierPredictions = predictor.Predict(elementSDR);
                     //string log = "";
 
@@ -335,11 +148,9 @@ namespace ProjectMultiSequenceLearning
                     //{
                     //    Debug.WriteLine($"Predicted Input: {pred.PredictedInput} - Similarity: {pred.Similarity}%");
                     //}
-                    Console.WriteLine($"Classifier Predictions Count {classifierPredictions.Count}");
+                    //Console.WriteLine($"Classifier Predictions Count {classifierPredictions.Count}");
                     foreach (var cp in classifierPredictions)
                     {
-                        //if (i < userInput.Length - 1)
-                        //{
                         var prediction = cp.PredictedInput.Split(",");
                         if (Convert.ToChar(prediction.First()) == seqItem)
                         {
@@ -356,7 +167,6 @@ namespace ProjectMultiSequenceLearning
                             }
                             break;
                         }
-                        // }
                     }
 
                     //var sequence = res.First().PredictedInput.Split('_');
@@ -406,8 +216,9 @@ namespace ProjectMultiSequenceLearning
              */
             accuracy = (double)matchCount / predictions * 100;
             //report.Accuracy = accuracy;
-            Console.WriteLine("------------------------------");
 
+            Console.WriteLine("------------------------------");
+            Console.WriteLine($"Sequence {sequenceData}");
             var predictedSequenceMode = possibleModes.GroupBy(x => x.Split("_")[0])
                    .Select(g => new { possibleMode = g.Key, Count = g.Count() })
                    .ToList()
@@ -421,5 +232,6 @@ namespace ProjectMultiSequenceLearning
 
             return accuracy;
         }
+
     }
 }
