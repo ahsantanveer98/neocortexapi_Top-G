1157c419c5a42541b14b51fd54edcc08c20907f5
Babar-Ali-IT <116679936+Babar-Ali-IT@users.noreply.github.com>
2024-03-23 02:14:00 +0100

    Created HTM_Model class

---
+++ source/MySEProject/MultiSequenceLearning/HTM_Model.cs
@@ -0,0 +1,14 @@
+﻿using NeoCortexApi.Classifiers;
+using NeoCortexApi.Entities;
+using NeoCortexApi.Network;
+using System;
+
+namespace ProjectMultiSequenceLearning
+{
+    public class HTM_Model
+    {
+       public  CortexLayer<object, object> CortexLayer { get; set; }
+
+        public HtmClassifier<string, ComputeCycle> HtmClassifier { get; set; }
+    }
+}
