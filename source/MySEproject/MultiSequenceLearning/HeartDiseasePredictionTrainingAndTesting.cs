0146e4ebc55564a8a2e6d6862fd21651ed7dd85c
Waheed khan <waheedkhn65@gmail.com>
2024-03-26 00:29:31 +0100

    project Code for Senario-3_HeartDisease Prediction Completed

---
+++ source/MySEproject/MultiSequenceLearning/HeartDiseasePredictionTrainingAndTesting.cs
@@ -0,0 +1,115 @@
+﻿using NeoCortexApi;
+using System;
+
+namespace ProjectMultiSequenceLearning
+{
+    public class HeartDiseasePredictionTrainingAndTesting
+    {
+        /// <summary>
+        /// Runs MultiSequence Learning Experiment - To Carry out Sequence Learning for Heart Disease Scenario.
+        /// </summary>
+        /// <param name="predictionScenario"></param>
+        /// <param name="trainingDataFilePath"></param>
+        /// <param name="testingDataFilePath"></param>
+        public void RunMultiSequenceLearningExperiment(int predictionScenario, string trainingDataFilePath, string testingDataFilePath)
+        {
+            var trainingData = Helper.ReadSequencesDataFromCSV(trainingDataFilePath);
+            var encodeTrainingData = Helper.TrainEncodeSequencesFromCSV(trainingData, predictionScenario);
+
+            Console.WriteLine("Variables are being trained Please Wait....");
+            /// <summary>
+            /// Prototype for building the prediction engine.
+            ///  </summary>
+            MultiSequenceLearning experiment = new MultiSequenceLearning();
+            var predictor = experiment.Run(encodeTrainingData);
+
+            Console.WriteLine("Ready to Predict.....");
+            var testingData = Helper.ReadTestingSequencesDataFromCSV(testingDataFilePath);
+
+            foreach (var seqData in testingData)
+            {
+                Console.WriteLine("------------------------------");
+                Console.WriteLine($"Sequence {seqData}");
+                predictor.Reset();
+                var accuracy = PredictElementAccuracy(predictor, seqData, predictionScenario);
+                Console.WriteLine($"Accuracy {accuracy}%");
+            }
+            Console.WriteLine("------------------------------");
+        }
+
+        /// <summary>
+        /// Takes predicted model, subsequence and predict accuracy
+        /// </summary>
+        /// <param name="predictor">Object of Predictor</param>
+        /// <param name="sequenceData">sequence to be tested</param>
+        /// <param name="predictionScenario">Prediction Scenario</param>
+        /// <returns>accuracy of predicting elements in %</returns>
+        private static object PredictElementAccuracy(Predictor predictor, string sequenceData, int predictionScenario)
+        {
+            int matchCount = 0;
+            int predictions = 0;
+            double accuracy = 0.0;
+            char prevSeqItem = ' ';
+            bool first = true;
+
+            List<string> possibleModes = new List<string>();
+            Console.WriteLine("------------------------------");
+
+            foreach (var seqItem in sequenceData)
+            {
+                if (first)
+                {
+                    first = false;
+                }
+                else
+                {
+                    Console.WriteLine($"Element {prevSeqItem}");
+                    var elementSDR = Helper.EncodeTestingElement(prevSeqItem.ToString(), predictionScenario);
+                    Thread.Sleep(1000); 
+                    var classifierPredictions = predictor.Predict(elementSDR);
+                    
+                    foreach (var cp in classifierPredictions)
+                    {
+                        var predictedSequence = cp.PredictedInput.Split('_');
+                        var predictedElements = predictedSequence.Last().Split('-');
+
+                        if (Convert.ToChar(predictedElements.Last()) == seqItem)
+                        {
+                            Console.WriteLine($"Predicted next element: {predictedElements.Last()}");
+                            matchCount++;
+
+                            var prediction = cp.PredictedInput.Split(",");
+                            if (prediction.Length >= 3)
+                            {
+                                possibleModes.Add(prediction[2]);
+                            }
+                            else
+                            {
+                                possibleModes.Add(prediction[1]);
+                            }
+                            break;
+                        }
+                    }
+                    predictions++;
+                }
+
+                //save previous element to compare with upcoming element
+                prevSeqItem = seqItem;
+            }
+            accuracy = (double)matchCount / predictions * 100;
+
+            Console.WriteLine("------------------------------");
+            Console.WriteLine($"Sequence {sequenceData}");
+
+            var predictedSequenceMode = possibleModes.GroupBy(x => x.Split("_")[0])
+                   .Select(g => new { possibleMode = g.Key, Count = g.Count() })
+                   .ToList()
+                   .OrderByDescending(x => x.Count).FirstOrDefault();
+
+            if (predictedSequenceMode != null)
+                Console.WriteLine($"Heart Disease Status : " + (predictedSequenceMode.possibleMode == "1" ? "Active" : "Inactive"));
+
+            return accuracy;
+        }
+    }
+}
--- source/MySEproject/MultiSequenceLearning/ProjectStarter.cs
+++ source/MySEproject/MultiSequenceLearning/ProjectStarter.cs
@@ -6,8 +6,24 @@ using System.Threading.Tasks;
 
 namespace ProjectMultiSequenceLearning
 {
-    internal class ProjectStarter
+    public class ProjectStarter
     {
+        /// <summary>
+        /// Training File Paths For Sequences
+        /// </summary>
+        ///
+#pragma warning disable CS8602 //disable the warning 
+        static readonly string FilePath = Path.GetFullPath(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName);
+
+        static readonly string CancerTrainingDataFile = FilePath + @"\TrainingData\CancerTrainingData.csv";
+        static readonly string HeartDiseaseTrainingDataFile = FilePath + @"\TrainingData\HeartDiseaseTrainingData.csv";
+        static readonly string PowerConsumptionTrainingDataFile = FilePath + @"\TrainingData\PowerTrainingData.csv";
+
+        static readonly string CancerTestingDataFile = FilePath + @"\TestingData\CancerTestingData.csv";
+        static readonly string HeartDiseaseTestingDataFile = FilePath + @"\TestingData\HeartDiseaseTestingData.csv";
+        static readonly string PowerConsumptionTestingDataFile = FilePath + @"\TestingData\PowerTestingData.csv";
+
+
         /// <summary>
         /// Print Message During Startup of Program
         /// </summary>
@@ -15,8 +31,8 @@ namespace ProjectMultiSequenceLearning
         public static void startingproject()
         {
             Console.WriteLine("\n");
-            // Set the Foreground color to green
-            Console.ForegroundColor = ConsoleColor.Green;
+            // Set the Foreground color to blue
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("######## ########    ###    ##     ##    ########   ####     #########             ######    ");
             Console.WriteLine("   ##    ##         ## ##   ###   ###       ##     ##  ##    ##      ##           ##         ");
             Console.WriteLine("   ##    ##        ##   ##  #### ####       ##    ##    ##   ##      ##          ##         ");
@@ -27,18 +43,18 @@ namespace ProjectMultiSequenceLearning
 
             Console.WriteLine("\n\n\n");
 
-            // Set the Foreground color to dark yellow
+            // Set the Foreground color to blue
             Console.ForegroundColor = ConsoleColor.DarkYellow;
 
             Console.WriteLine("**************          Approve Prediction of Multi Sequence Learning               ************** \n ");
             Console.WriteLine("**************  Scenario - 1 - Cancer_Prediction     ************** ");
-            Console.WriteLine("**************  Scenario - 2 - Power_Prediction   ************** ");
+            Console.WriteLine("**************  Scenario - 2 - Power_Consumption_Prediction   ************** ");
             Console.WriteLine("**************  Scenario - 3 - Heart_Disease_Prediction      ************** ");
 
             Console.WriteLine("\n");
-            Console.WriteLine("Please Enter a Scenario Number to Continue with MultiSequence Experiment");
+            Console.WriteLine("Please Enter A Scenario Number to Continue with MultiSequence Experiment");
 
-            // Set the Foreground color to white
+            // Set the Foreground color to blue
             Console.ForegroundColor = ConsoleColor.White;
         }
 
@@ -47,40 +63,29 @@ namespace ProjectMultiSequenceLearning
         /// </summary>
         /// <param name="UserInput"></param>
 
-        public static void UserSelection(int UserInput)
+        public static void UserSelection(int userInput)
         {
-            switch (UserInput)
+            switch (userInput)
             {
                 case 1:
-
                     Console.WriteLine("User Selected MultiSequence Experiment - Cancer_Prediction\n");
-
-
+                   // CancerPredictionTrainingAndTesting cancerPrediction = new CancerPredictionTrainingAndTesting();
+                    //cancerPrediction.RunMultiSequenceLearningExperiment(userInput, CancerTrainingDataFile, CancerTestingDataFile);
                     break;
-
                 case 2:
-
-                    Console.WriteLine("User Selected MultiSequence Experiment - Power_Prediction\n");
-
-
+                    Console.WriteLine("User Selected MultiSequence Experiment - Power_Consumption_Prediction\n");
+                    //PowerConsumptionPredictionTrainingAndTesting powerConsumptionPrediction = new PowerConsumptionPredictionTrainingAndTesting();
+                    //powerConsumptionPrediction.RunMultiSequenceLearningExperiment(userInput, PowerConsumptionTrainingDataFile, PowerConsumptionTestingDataFile);
                     break;
-
                 case 3:
-
                     Console.WriteLine("User Selected MultiSequence Experiment - Heart_Disease_Prediction\n");
-
-                    var trainingData = HeartDiseasePredictionTraining.ReadSequencesDataFromCSV(HeartDiseaseTrainingDataFile);
-                    var trainingDataProcessed = HeartDiseasePredictionTraining.TrainEncodeSequencesFromCSV(trainingData);
-
+                    HeartDiseasePredictionTrainingAndTesting heartDiseasePrediction = new HeartDiseasePredictionTrainingAndTesting();
+                    heartDiseasePrediction.RunMultiSequenceLearningExperiment(userInput, HeartDiseaseTrainingDataFile, HeartDiseaseTestingDataFile);   
                     break;
-
                 default:
-
                     Console.WriteLine("User Entered Invalid Option");
-
                     break;
             }
         }
-
     }
 }
