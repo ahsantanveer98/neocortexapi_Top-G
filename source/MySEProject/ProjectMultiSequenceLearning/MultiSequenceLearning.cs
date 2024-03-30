eb68887b07ee0dd510b1a9b1407aa9d17d2ae141
unknown <muhammad.tanveer@stud.fra-uas.de>
2024-03-30 21:58:30 +0100

    update

---
+++ source/MySEProject/ProjectMultiSequenceLearning/MultiSequenceLearning.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Linq;
+
+
+using NeoCortexApi;
+using NeoCortexApi.Classifiers;
+using NeoCortexApi.Encoders;
+using NeoCortexApi.Entities;
+using NeoCortexApi.Network;
+using System.Text.RegularExpressions;
+
+
+
+namespace ProjectMultiSequenceLearning
+{
+    /// <summary>
+    /// Implements an experiment that demonstrates how to learn sequences.
+    /// </summary>
+    public class MultiSequenceLearning
+    {
+        /// <summary>
+        /// Runs the learning of sequences.
+        /// </summary>
+        /// <param name="sequences">Dictionary of sequences. KEY is the sequence name, the VALUE is th elist of element of the sequence.</param>
+        public Predictor Run(List<Dictionary<string, int[]>> sequences)
+        {
+            int inputBits = 200;
+            int numColumns = 1024;
+
+            HtmConfig cfg = Helper.FetchHTMConfig(inputBits, numColumns);
+            return RunExperiment(inputBits, cfg, sequences);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        private Predictor RunExperiment(int inputBits, HtmConfig cfg, List<Dictionary<string, int[]>> sequences)
+        {
+            int maxCycles = 35;
+            int maxMatchCnt = 0;
+            //bool classVotingEnabled = true;
+
+            // Starting experiment
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            //--------- CONNECTIONS
+            var mem = new Connections(cfg);
+
+            // HTM CLASSIFIER
+            HtmClassifier<string, ComputeCycle> cls = new HtmClassifier<string, ComputeCycle>();
+
+            // CORTEX LAYER
+            CortexLayer<object, object> layer1 = new CortexLayer<object, object>("L1");
+
+            // HPA IS_IN_STABLE STATE FLAG
+            bool isInStableState = false;
+
+            // LEARNING ACTIVATION FLAG
+            bool learn = true;
+
+            // NUMBER OF NEW BORN CYCLES
+            int newbornCycle = 0;
+
+            // HOMOSTATICPLASTICITY CONTROLLER
+            HomeostaticPlasticityController hpa = new HomeostaticPlasticityController(mem, sequences.Count * 150, (isStable, numPatterns, actColAvg, seenInputs) =>
+            {
+                if (isStable)
+                    // Event should be fired when entering the stable state.
+                    Debug.WriteLine($"STABLE: Patterns: {numPatterns}, Inputs: {seenInputs}, iteration: {seenInputs / numPatterns}");
+                else
+                    // Ideal SP should never enter unstable state after stable state.
+                    Debug.WriteLine($"INSTABLE: Patterns: {numPatterns}, Inputs: {seenInputs}, iteration: {seenInputs / numPatterns}");
+
+                // We are not learning in instable state.
+                isInStableState = isStable; //learn = isInStableState = isStable;
+
+                // Clear all learned patterns in the classifier.
+                //cls.ClearState();
+
+            }, numOfCyclesToWaitOnChange: 50);
+
+            // SPATIAL POOLER initialization with HomoPlassiticityController using connections.
+            SpatialPoolerMT sp = new SpatialPoolerMT(hpa);
+            sp.Init(mem);
+
+            // TEMPORAL MEMORY initialization using connections.
+            TemporalMemory tm = new TemporalMemory();
+            tm.Init(mem);
+
+            //layer1.HtmModules.Add("encoder", encoder);
+            // ADDING SPATIAL POOLER TO CORTEX LAYER
+            layer1.HtmModules.Add("sp", sp);
+
+            // CONTRAINER FOR Previous Active Columns
+            int[] prevActiveCols = new int[0];
+
+            // TRAINING SP till STATBLE STATE IS ACHIEVED
+            while (isInStableState == false) // STABLE CONDITION LOOP ::: LOOP - 0
+            {
+                newbornCycle++;
+                Debug.WriteLine($"-------------- Newborn Cycle {newbornCycle} ---------------");
+                Console.WriteLine($"-------------- Newborn Cycle {newbornCycle} ---------------");
+
+                foreach (var sequence in sequences) // FOR EACH SEQUENCE IN SEQUNECS LOOP ::: LOOP - 1
+                {
+                    foreach (var element in sequence) // FOR EACH dictionary containing single sequence Details LOOP ::: LOOP - 2
+                    {
+                        var observationClass = element.Key; // OBSERVATION LABEL || SEQUENCE LABEL
+                        var elementSDR = element.Value; // ALL ELEMENT IN ONE SEQUENCE 
+
+                        Console.WriteLine($"-------------- {observationClass} ---------------");
+                        // CORTEX LAYER OUTPUT with elementSDR as INPUT and LEARN = TRUE
+                        var lyrOut = layer1.Compute(elementSDR, learn);
+
+                        // IF STABLE STATE ACHIEVED BREAK LOOP - 3
+                        if (isInStableState)
+                            break;
+
+                    }
+                    if (isInStableState)
+                        break;
+                }
+            }
+
+            // Clear all learned patterns in the classifier.
+            cls.ClearState();
+
+            // ADDING TEMPORAL MEMEORY to CORTEX LAYER
+            layer1.HtmModules.Add("tm", tm);
+
+            //string lastPredictedValue = "-1";
+            //double lastCycleAccuracy = 0;
+            //double accuracy = 0;
+
+            //List<List<string>> possibleSequence = new List<List<string>>();
+
+            List<string> lastPredictedValues = new List<string>();
+
+            // TRAINING SP+TM TOGETHER
+            foreach (var sequence in sequences)  // SEQUENCE LOOP
+            {
+                //Debug.WriteLine($"-------------- Sequences {sequence.Key} ---------------");
+                int cycle = 0;
+                int maxPrevInputs = sequence.Count;
+
+                // Set on true if the system has learned the sequence with a maximum acurracy.
+                bool isLearningCompleted = false;
+
+                for (int i = 0; i < maxCycles; i++) // MAXCYCLE LOOP 
+                {
+                    List<string> previousInputs = new List<string>();
+                    cycle++;
+                    int elementMatches = 0; // ELEMENT IN SEQUENCE MATCHES COUNT
+                    Debug.WriteLine($"-------------- Cycle SP+TM{cycle} ---------------");
+                    Console.WriteLine($"-------------- Cycle SP+TM {cycle} ---------------");
+
+                    foreach (var elements in sequence) // SEQUENCE DICTIONARY LOOP
+                    {
+                        Debug.WriteLine($"-------------- Sequence Element {elements.Key} ---------------");
+                        //Console.WriteLine($"-------------- Sequence Element {elements.Key} ---------------");
+                        var observationLabel = elements.Key; // OBSERVATION LABEl
+                        var elementSdr = elements.Value; // ELEMENT SDR LIST FOR A SINGLE SEQUENCE
+
+                        var observationLabels = observationLabel.Split(",");
+                        var element = observationLabels[0];
+
+                        var lyrOut = layer1.Compute(elementSdr, learn) as ComputeCycle;
+                        if (lyrOut != null)
+                        {
+                            Debug.WriteLine(string.Join(',', lyrOut.ActivColumnIndicies));
+                            var activeColumns = layer1.GetResult("sp") as int[];
+                            previousInputs.Add(element);
+
+                            //// In the pretrained SP with HPC, the TM will quickly learn cells for patterns
+                            //// In that case the starting sequence 4-5-6 might have the sam SDR as 1-2-3-4-5-6,
+                            //// Which will result in returning of 4-5-6 instead of 1-2-3-4-5-6.
+                            //// HtmClassifier allways return the first matching sequence. Because 4-5-6 will be as first
+                            //// memorized, it will match as the first one.
+                            //if (previousInputs.Count < maxPrevInputs)
+                            //    continue;
+
+                            string key = GetKey(previousInputs, element, observationLabel);
+
+                            List<Cell> actCells = new List<Cell>();
+                            // Active Cells
+                            actCells = (lyrOut.ActiveCells.Count == lyrOut.WinnerCells.Count) ? lyrOut.ActiveCells : lyrOut.WinnerCells;
+
+                            cls.Learn(key, actCells.ToArray());
+
+                            Debug.WriteLine($"Col  SDR: {Helpers.StringifyVector(lyrOut.ActivColumnIndicies)}");
+                            Debug.WriteLine($"Cell SDR: {Helpers.StringifyVector(actCells.Select(c => c.Index).ToArray())}");
+
+                            // If the list of predicted values from the previous step contains the currently presenting value,
+                            // we have a match.
+                            if (lastPredictedValues.Contains(key))
+                            {
+                                elementMatches++;
+                                Debug.WriteLine($"Match. Actual value: {key} - Predicted value: {lastPredictedValues.FirstOrDefault(key)}.");
+                            }
+                            else
+                            {
+                                Debug.WriteLine($"Missmatch! Actual value: {key} - Predicted values: {String.Join(',', lastPredictedValues)}");
+                            }
+
+                            if (lyrOut.PredictiveCells.Count > 0)
+                            {
+                                var predictedInputValues = cls.GetPredictedInputValues(lyrOut.PredictiveCells.ToArray(), 5);
+                                foreach (var predictedInputValue in predictedInputValues)
+                                {
+                                    Debug.WriteLine($"Current Input: {key} \t| Predicted Input: {predictedInputValue.PredictedInput} - {predictedInputValue.Similarity}");
+                                }
+
+                                lastPredictedValues = predictedInputValues.Select(v => v.PredictedInput).ToList();
+                            }
+                            else
+                            {
+                                Debug.WriteLine($"NO CELLS PREDICTED for next cycle.");
+                                lastPredictedValues = new List<string>();
+                            }
+                        }
+                    }
+
+                    // The first element (a single element) in the sequence cannot be predicted
+                    double maxPossibleAccuraccy = (double)((double)sequence.Count - 1) / (double)sequence.Count * 100.0;
+
+                    double accuracy = (double)elementMatches / (double)sequence.Count * 100.0;
+
+                    Debug.WriteLine($"Cycle: {cycle}\tMatches={elementMatches} of {sequence.Count}\t {accuracy}%");
+                    Console.WriteLine($"Cycle: {cycle}\tMatches={elementMatches} of {sequence.Count}\t {accuracy}%");
+
+                    if (accuracy >= maxPossibleAccuraccy)
+                    {
+                        maxMatchCnt++;
+                        Debug.WriteLine($"100% accuracy reched {maxMatchCnt} times.");
+
+                        //
+                        // Experiment is completed if we are 30 cycles long at the 100% accuracy.
+                        if (maxMatchCnt >= 30)
+                        {
+                            sw.Stop();
+                            Debug.WriteLine($"Sequence learned. The algorithm is in the stable state after 30 repeats with with accuracy {accuracy} of maximum possible {maxMatchCnt}. learning time: {sw.Elapsed}.");
+                            isLearningCompleted = true; 
+                            break;
+                        }
+                    }
+                    else if (maxMatchCnt > 0)
+                    {
+                        Debug.WriteLine($"At 100% accuracy after {maxMatchCnt} repeats we get a drop of accuracy with accuracy {accuracy}. This indicates instable state. Learning will be continued.");
+                        maxMatchCnt = 0;
+                    }
+
+                    // This resets the learned state, so the first element starts allways from the beginning.
+                    tm.Reset(mem);
+                    //lastPredictedValues.Clear();
+                }
+
+                //if (isLearningCompleted == false)
+                //    throw new Exception($"The system didn't learn with expected acurracy!");
+            }
+
+            //sw.Stop();
+
+            //****************DISPLAY STATUS OF EXPERIMENT
+            Debug.WriteLine("-------------------TRAINING END------------------------");
+            Console.WriteLine("-------------------TRAINING END------------------------");
+
+            return new Predictor(layer1, mem, cls);
+        }
+
+        /// <summary>
+        /// Gets the number of all unique inputs.
+        /// </summary>
+        /// <param name="sequences">Alle sequences.</param>
+        /// <returns></returns>
+        private int GetNumberOfInputs(Dictionary<string, List<double>> sequences)
+        {
+            int num = 0;
+
+            foreach (var inputs in sequences)
+            {
+
+                num += inputs.Value.Count;
+            }
+
+            return num;
+        }
+
+        /// <summary>
+        /// Constracts the unique key of the element of an sequece. This key is used as input for HtmClassifier.
+        /// It makes sure that alle elements that belong to the same sequence are prefixed with the sequence.
+        /// The prediction code can then extract the sequence prefix to the predicted element.
+        /// </summary>
+        /// <param name="prevInputs"></param>
+        /// <param name="input"></param>
+        /// <param name="sequence"></param>
+        /// <returns></returns>
+        private static string GetKey(List<string> prevInputs, string input, string sequence)
+        {
+            string key = String.Empty;
+
+            for (int i = 0; i < prevInputs.Count; i++)
+            {
+                if (i > 0)
+                    key += "-";
+
+                key += (prevInputs[i]);
+            }
+
+            return $"{sequence}_{key}";
+        }
+
+    }
+}
---
+++ source/MySEProject/ProjectMultiSequenceLearning/PowerConsumptionPredictionTrainingAndTesting.cs
@@ -0,0 +1,142 @@
+﻿using NeoCortexApi;
+using System;
+using System.Diagnostics;
+
+namespace ProjectMultiSequenceLearning
+{
+    public class PowerConsumptionPredictionTrainingAndTesting
+    {
+        /// <summary>
+        /// Runs MultiSequence Learning Experiment - To Carry out Sequence Learning for Heart Disease Scenario.
+        /// </summary>
+        /// <param name="predictionScenario"></param>
+        /// <param name="trainingDataFilePath"></param>
+        /// <param name="testingDataFilePath"></param>
+        public void RunMultiSequenceLearningExperiment(int predictionScenario, string trainingDataFilePath, string testingDataFilePath)
+        {
+            //Get Sequence Data from file for training
+            var trainingData = Helper.ReadSequencesDataFromCSVForPC(trainingDataFilePath);
+
+            //Encode Sequence Training Data
+            var encodeTrainingData = Helper.TrainEncodeSequencesFromCSV(trainingData, predictionScenario);
+
+            Console.WriteLine("Variables are being trained Please Wait....");
+
+            /// <summary>
+            /// Prototype for building the prediction engine.
+            ///  </summary>
+            MultiSequenceLearning experiment = new MultiSequenceLearning();
+            var predictor = experiment.Run(encodeTrainingData);
+
+            Console.WriteLine("Ready to Predict.....");
+
+            //Get Testing Sequence Data from file
+            var testingData = Helper.ReadTestingSequencesDataFromCSV(testingDataFilePath);
+
+            //Testing Data Execute Row by Row
+            foreach (var seqData in testingData)
+            {
+                Console.WriteLine($"Sequence {seqData}");
+                predictor.Reset();
+                var accuracy = PredictElementAccuracy(predictor, seqData, predictionScenario);
+                Console.WriteLine($"Accuracy {accuracy}%");
+            }
+        }
+
+        /// <summary>
+        /// Takes predicted model, subsequence and predict accuracy
+        /// </summary>
+        /// <param name="predictor">Object of Predictor</param>
+        /// <param name="sequenceData">sequence to be tested</param>
+        /// <param name="predictionScenario">Prediction Scenario</param>
+        /// <returns>accuracy of predicting elements in %</returns>
+        private static double PredictElementAccuracy(Predictor predictor, string sequenceData, int predictionScenario)
+        {
+            int matchCount = 0;
+            int predictions = 0;
+            double accuracy = 0.0;
+            string prevSeqItem = "";
+            bool first = true;
+
+            List<string> possibleModes = new List<string>();
+            Console.WriteLine("------------------------------");
+            
+            //split the data by comma
+            var sequenceDataList = sequenceData.Split(",");
+
+            //Every Row is Divide into single element for predict next element and calculate accuuracy
+            foreach (var seqItem in sequenceDataList)
+            {
+                if (first)
+                {
+                    first = false;
+                }
+                else
+                {
+                    Console.WriteLine($"Element {prevSeqItem}");
+                    var elementSDR = Helper.EncodeTestingElement(prevSeqItem, predictionScenario);
+                    Thread.Sleep(1000);
+                    var classifierPredictions = predictor.Predict(elementSDR);
+                    Console.WriteLine($"Classifier Predictions Count {classifierPredictions.Count}");
+                    
+
+                    foreach (var cp in classifierPredictions)
+                    {
+                        
+
+                        var predictedSequence = cp.PredictedInput.Split('_');
+                        var predictedElements = predictedSequence.Last().Split('-');
+                        
+                        if (predictedElements.Last() == seqItem)
+                        {
+                            Console.WriteLine($"Predicted next element: {predictedElements.Last()}");
+                            matchCount++;
+
+                            var prediction = cp.PredictedInput.Split(",");
+                            if (prediction.Length >= 3)
+                            {
+                                possibleModes.Add(prediction[2]);
+                            }
+                            else
+                            {
+                                possibleModes.Add(prediction[1]);
+                            }
+                            break;
+                        }
+
+                    }
+                    predictions++;
+                }
+
+                //save previous element to compare with upcoming element
+                prevSeqItem = seqItem;
+            }
+
+            #region CalCulate Accuracy and Predict Mode
+
+            /*
+            * Accuracy is calculated as number of matching predictions made 
+            * divided by total number of prediction made for an element in subsequence
+            * 
+            * accuracy = number of matching predictions/total number of prediction * 100
+            */
+
+            accuracy = (double)matchCount / predictions * 100;
+            Console.WriteLine("------------------------------");
+
+            //Predict Mode
+
+            var predictedSequenceMode = possibleModes.GroupBy(x => x.Split("_")[0])
+                   .Select(g => new { possibleMode = g.Key, Count = g.Count() })
+                   .ToList()
+                   .OrderByDescending(x => x.Count).FirstOrDefault();
+
+            if (predictedSequenceMode != null)
+                Console.WriteLine($"Power Consumption : {predictedSequenceMode.possibleMode}");
+
+            #endregion
+
+            return accuracy;
+        }
+    }
+}
